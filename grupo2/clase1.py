8412baf923364aafbcae578f9aa6a889eaa59bbb
John Salazar Pinto <johnpiterzon@gmail.com>
2020-05-16 21:33:57 -0400

    Ejemplo uso de metodos.

--- grupo2/clase1.py
+++ grupo2/clase1.py
@@ -1,6 +1,5 @@
 """Clase 1
 """
-
 altura = int(input("Ingrese la altura de la seccion: "))
 base = int(input("Ingrese la base de la seccion: "))
 distancia = int(input("Ingrese la distancia del rio: "))
@@ -21,8 +20,54 @@ def calcular_velocidad(distancia, tiempo):
     velocidad = distancia / tiempo
     return velocidad
 
+def calcular_area(altura, base):
+    """Este metodo calcula el area
 
-print(calcular_velocidad(distancia, tiempo))
-print(calcular_velocidad(50, 20))
-print(calcular_velocidad(50, 10))
-print(calcular_velocidad(50, 50))
+    Arguments:
+        altura {int} -- Altura en metros
+        base {int} -- Ancho del rio en metros
+    Returns:
+        int -- area en m2
+    """
+    area = altura * base
+    return area
+
+velocidad = calcular_velocidad(distancia, tiempo)
+area = calcular_area(altura, base)
+
+
+def calcular_caudal(velocidad, area):
+    """Este metodo calcula el caudal
+
+    Arguments:
+        velocidad {int} -- velocidad en metros sobre segundo
+        area {int} -- area en metros cuadrados
+    Returns:
+        int -- caudal en metros cubicos sobre segundo
+    """
+
+    caudal = velocidad * area
+    return caudal
+
+
+print("El caudal es:", calcular_caudal(velocidad, area))
+
+
+def calculo_estabilidad_modelo(delta_x, delta_t):
+    """Este metodo calcula la estabilidad
+
+    Arguments:
+        delta_x {int} -- Distancia de nodo a nodo en el dominio x
+        delta_t {int} -- Paso de tiempo
+
+    Returns:
+        int -- Estabilidad a adoptar
+    """
+    estabilidad = delta_x / delta_t
+    if estabilidad > 1:
+        estabilidad = 1
+    else:
+        pass
+    return estabilidad
+
+print(calculo_estabilidad_modelo(40, 20))
