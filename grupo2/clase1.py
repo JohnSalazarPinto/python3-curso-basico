e7ee50b9174a66cbba972bfdef91ae3a601319a9
John Salazar Pinto <johnpiterzon@gmail.com>
2020-05-16 20:55:23 -0400

    calculo_velocidad.

--- grupo2/clase1.py
+++ grupo2/clase1.py
@@ -1,26 +1,28 @@
 """Clase 1
 """
 
-altura = input("Ingrese la altura de la seccion: ")
-base = input("Ingrese la base de la seccion: ")
-distancia = input("Ingrese la distancia del rio: ")
-tiempo = input("Ingrese el timpo del flujo: ")
+altura = int(input("Ingrese la altura de la seccion: "))
+base = int(input("Ingrese la base de la seccion: "))
+distancia = int(input("Ingrese la distancia del rio: "))
+tiempo = int(input("Ingrese el timpo del flujo: "))
 
-print(altura)
-print(base)
-print(distancia)
-print(tiempo)
 
-def velocidad(distancia, tiempo):
+def calcular_velocidad(distancia, tiempo):
     """Este metodo calcula la velocidad
 
     Arguments:
-        distancia {int} -- Distancia del rio
-        tiempo {int} -- Tiempo que tarda la botella en recorrer
-                        la distancia del rio
+        distancia {int} -- Distancia del rio en metros
+        tiempo {int} -- Tiempo en segundos que tarda la botella en recorrer
+                        la distancia del rio 
 
     Returns:
         int -- Velocidad en m/s
     """
-    velocidad = distancia * tiempo
+    velocidad = distancia / tiempo
     return velocidad
+
+
+print(calcular_velocidad(distancia, tiempo))
+print(calcular_velocidad(50, 20))
+print(calcular_velocidad(50, 10))
+print(calcular_velocidad(50, 50))
